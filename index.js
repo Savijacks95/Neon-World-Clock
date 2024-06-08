bc218712340cb37de140661e5ceadee2b1dd6c6c
Savannah Jackson <savannahjackson95@gmail.com>
2024-06-08 10:19:33 -0700

    added a function
    added set interval

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-let time = moment();
-let seattleElement = document.querySelector("#Seattle");
-let seattleDate = seattleElement.querySelector(".date");
-let seattleTime = seattleElement.querySelector(".digital-time");
-let seattleTimeZone = time.tz("America/Vancouver");
-seattleDate.innerHTML = time.format("MMMM Do, YYYY");
-seattleTime.innerHTML = seattleTimeZone.format("h:mm:ss [<small>]A[</small>]");
+function timeUpdate() {
+  let time = moment();
+  let seattleElement = document.querySelector("#Seattle");
+  let seattleDate = seattleElement.querySelector(".date");
+  let seattleTime = seattleElement.querySelector(".digital-time");
+  let seattleTimeZone = time.tz("America/Vancouver");
+  seattleDate.innerHTML = time.format("MMMM Do, YYYY");
+  seattleTime.innerHTML = seattleTimeZone.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
-let delhiElement = document.querySelector("#Delhi");
-let delhiDate = delhiElement.querySelector(".date");
-let delhiTime = delhiElement.querySelector(".digital-time");
-let delhiTimeZone = time.tz("Asia/Kolkata");
-delhiDate.innerHTML = time.format("MMMM Do, YYYY");
-delhiTime.innerHTML = delhiTimeZone.format("h:mm:ss [<small>]A[</small>]");
+  let delhiElement = document.querySelector("#Delhi");
+  let delhiDate = delhiElement.querySelector(".date");
+  let delhiTime = delhiElement.querySelector(".digital-time");
+  let delhiTimeZone = time.tz("Asia/Kolkata");
+  delhiDate.innerHTML = time.format("MMMM Do, YYYY");
+  delhiTime.innerHTML = delhiTimeZone.format("h:mm:ss [<small>]A[</small>]");
 
-let zurichElement = document.querySelector("#Zurich");
-let zurichDate = zurichElement.querySelector(".date");
-let zurichTime = zurichElement.querySelector(".digital-time");
-let zurichTimeZone = time.tz("Europe/Zurich");
-zurichDate.innerHTML = time.format("MMMM Do, YYYY");
-zurichTime.innerHTML = zurichTimeZone.format("h:mm:ss [<small>]A[</small>]");
-
-let monthElement = document.querySelector(".month");
-monthElement.innerHTML = time.format("MMMM Do, YYYY");
+  let zurichElement = document.querySelector("#Zurich");
+  let zurichDate = zurichElement.querySelector(".date");
+  let zurichTime = zurichElement.querySelector(".digital-time");
+  let zurichTimeZone = time.tz("Europe/Zurich");
+  zurichDate.innerHTML = time.format("MMMM Do, YYYY");
+  zurichTime.innerHTML = zurichTimeZone.format("h:mm:ss [<small>]A[</small>]");
+  let monthElement = document.querySelector(".month");
+  monthElement.innerHTML = time.format("MMMM Do, YYYY");
+}
+setInterval(timeUpdate, 1000);
